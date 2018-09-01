e1ae48844ff7e2b618801455d64dc3b1fd5660bd
lorenz <looooo@users.noreply.github.com>
2018-09-01 23:40:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from freecad.template_extension import __version__
 # name: this is the name pip is using.
 # Packages using the same name here cannot be installed together
 
-setup(name='template-extension',
+setup(name='freecad.starterkit',
       version=str(__version__),
       packages=['freecad',
                 'freecad.template_extension'],
       maintainer="looooo",
       maintainer_email="sppedflyer@gmail.com",
-      url="https://github.com/looooo/TemplateExtension",
-      description="template for freecad extensions, installable with pip",
-      install_requires=['numpy'],
+      url="https://github.com/FreeCAD/Workbench-Starterkit",
+      description="template for a freecad extensions, installable with pip",
+      install_requires=['numpy'], # should be satisfied by FreeCAD already
       include_package_data=True)
