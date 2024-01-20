2bdc80ab2c20cffbaac4b12ed65e53b652ab8db4
hasecilu <hasecilu@tuta.io>
2024-01-20 16:49:36 -0600

    Add support for translations
    - Add bash script to include marked strings on translation files (*.ts)
    - TranslateUtils to define translate() function
    - Spanish (es-ES) translation added as example

--- freecad/workbench_starterkit/init_gui.py
+++ freecad/workbench_starterkit/init_gui.py
@@ -1,16 +1,16 @@
 import os
 import FreeCADGui as Gui
 import FreeCAD as App
-from freecad.workbench_starterkit import ICONPATH
+from TranslateUtils import translate
+from freecad.workbench_starterkit import ICONPATH, TRANSLATIONSPATH
 
 
 class TemplateWorkbench(Gui.Workbench):
     """
     class which gets initiated at startup of the gui
     """
-
-    MenuText = "template workbench"
-    ToolTip = "a simple template workbench"
+    MenuText = translate("starterkit", "template workbench")
+    ToolTip = translate("starterkit", "a simple template workbench")
     Icon = os.path.join(ICONPATH, "template_resource.svg")
     toolbox = []
 
@@ -22,12 +22,18 @@ class TemplateWorkbench(Gui.Workbench):
         This function is called at the first activation of the workbench.
         here is the place to import all the commands
         """
-        from freecad.workbench_starterkit import my_numpy_function
-        App.Console.PrintMessage("switching to workbench_starterkit\n")
-        App.Console.PrintMessage("run a numpy function: sqrt(100) = {}\n".format(my_numpy_function.my_foo(100)))
+        # Add translations path
+        Gui.addLanguagePath(TRANSLATIONSPATH)
+        Gui.updateLocale()
 
-        self.appendToolbar("Tools", self.toolbox)
-        self.appendMenu("Tools", self.toolbox)
+        from freecad.workbench_starterkit import my_numpy_function
+        App.Console.PrintMessage(translate("Console",
+            "Switching to workbench_starterkit") + "\n")
+        App.Console.PrintMessage(translate("Console", "Run a numpy function:") \
+            + "sqrt(100) = {}\n".format(my_numpy_function.my_foo(100)))
+
+        self.appendToolbar(translate("Toolbar", "Tools"), self.toolbox)
+        self.appendMenu(translate("Menu", "Tools"), self.toolbox)
 
     def Activated(self):
         '''
