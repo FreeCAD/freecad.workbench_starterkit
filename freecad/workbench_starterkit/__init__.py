6fce3f599fd9bc65f50abce943287ca099d4e42c
hasecilu <hasecilu@tuta.io>
2024-01-20 16:49:36 -0600

    Add support for translations
    - Add bash script to include marked strings on translation files (*.ts)
    - TranslateUtils to define translate() function
    - Spanish (es-ES) translation added as example

--- freecad/workbench_starterkit/__init__.py
+++ freecad/workbench_starterkit/__init__.py
@@ -2,3 +2,4 @@ import os
 from .version import __version__
 
 ICONPATH = os.path.join(os.path.dirname(__file__), "resources")
+TRANSLATIONSPATH = os.path.join(os.path.dirname(__file__), "resources/translations")
---
+++ freecad/workbench_starterkit/resources/translations/workbench_starterkit_es-ES.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="en">
+<context>
+    <name>Console</name>
+    <message>
+        <location filename="../../init_gui.py" line="30"/>
+        <source>Switching to workbench_starterkit</source>
+        <translation>Cambiando a entorno de trabajo starterkit</translation>
+    </message>
+    <message>
+        <location filename="../../init_gui.py" line="32"/>
+        <source>Run a numpy function:</source>
+        <translation>Ejecutar una función de numpy</translation>
+    </message>
+</context>
+<context>
+    <name>Menu</name>
+    <message>
+        <location filename="../../init_gui.py" line="36"/>
+        <source>Tools</source>
+        <translation>Herramientas</translation>
+    </message>
+</context>
+<context>
+    <name>Toolbar</name>
+    <message>
+        <location filename="../../init_gui.py" line="35"/>
+        <source>Tools</source>
+        <translation>Herramientas</translation>
+    </message>
+</context>
+<context>
+    <name>starterkit</name>
+    <message>
+        <location filename="../../init_gui.py" line="12"/>
+        <source>template workbench</source>
+        <translation>Entorno de trabajo plantilla</translation>
+    </message>
+    <message>
+        <location filename="../../init_gui.py" line="13"/>
+        <source>a simple template workbench</source>
+        <translation>Una plantilla simple de entorno de trabajo</translation>
+    </message>
+</context>
+</TS>
