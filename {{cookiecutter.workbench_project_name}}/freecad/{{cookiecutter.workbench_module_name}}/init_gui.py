31f69821f01aad4b111e415a835d8c692948e4fd
hasecilu <hasecilu@tuta.io>
2024-10-19 10:01:40 -0600

    Upgrade translation support

--- {{cookiecutter.workbench_project_name}}/freecad/{{cookiecutter.workbench_module_name}}/init_gui.py
+++ {{cookiecutter.workbench_project_name}}/freecad/{{cookiecutter.workbench_module_name}}/init_gui.py
@@ -1,18 +1,24 @@
 import os
 import FreeCADGui as Gui
 import FreeCAD as App
-from freecad.{{cookiecutter.workbench_module_name}}.translate_utils import translate
 from freecad. {{cookiecutter.workbench_module_name}} import my_numpy_function
 
+translate=App.Qt.translate
+QT_TRANSLATE_NOOP=App.Qt.QT_TRANSLATE_NOOP
+
 ICONPATH = os.path.join(os.path.dirname(__file__), "resources")
-TRANSLATIONSPATH = os.path.join(os.path.dirname(__file__), "resources/translations")
+TRANSLATIONSPATH = os.path.join(os.path.dirname(__file__), "resources", "translations")
+
+# Add translations path
+Gui.addLanguagePath(TRANSLATIONSPATH)
+Gui.updateLocale()
 
 class {{cookiecutter.workbench_class_name}}(Gui.Workbench):
     """
     class which gets initiated at startup of the gui
     """
-    MenuText = translate("{{cookiecutter.workbench_module_name}}", "{{cookiecutter.workbench_menu_text}}")
-    ToolTip = translate("{{cookiecutter.workbench_module_name}}", "a simple {{cookiecutter.workbench_menu_text}}")
+    MenuText = translate("Workbench", "{{cookiecutter.workbench_menu_text}}")
+    ToolTip = translate("Workbench", "a simple {{cookiecutter.workbench_menu_text}}")
     Icon = os.path.join(ICONPATH, "{{cookiecutter.workbench_icon}}")
     toolbox = []
 
@@ -24,26 +30,24 @@ class {{cookiecutter.workbench_class_name}}(Gui.Workbench):
         This function is called at the first activation of the workbench.
         here is the place to import all the commands
         """
-        # Add translations path
-        Gui.addLanguagePath(TRANSLATIONSPATH)
-        Gui.updateLocale()
 
         App.Console.PrintMessage(translate(
-            "{{cookiecutter.workbench_module_name}}",
+            "Log",
             "Switching to {{cookiecutter.workbench_module_name}}") + "\n")
         App.Console.PrintMessage(translate(
-            "{{cookiecutter.workbench_module_name}}",
+            "Log",
             "Run a numpy function:") + "sqrt(100) = {}\n".format(my_numpy_function.my_foo(100)))
 
-        self.appendToolbar(translate("Toolbar", "Tools"), self.toolbox)
-        self.appendMenu(translate("Menu", "Tools"), self.toolbox)
+        # NOTE: Context for this commands must be "Workbench"
+        self.appendToolbar(QT_TRANSLATE_NOOP("Workbench", "Tools"), self.toolbox)
+        self.appendMenu(QT_TRANSLATE_NOOP("Workbench", "Tools"), self.toolbox)
 
     def Activated(self):
         '''
         code which should be computed when a user switch to this workbench
         '''
         App.Console.PrintMessage(translate(
-            "{{cookiecutter.workbench_module_name}}",
+            "Log",
             "Workbench {{cookiecutter.workbench_module_name}} activated.") + "\n")
 
     def Deactivated(self):
@@ -51,7 +55,7 @@ class {{cookiecutter.workbench_class_name}}(Gui.Workbench):
         code which should be computed when this workbench is deactivated
         '''
         App.Console.PrintMessage(translate(
-            "{{cookiecutter.workbench_module_name}}",
+            "Log",
             "Workbench {{cookiecutter.workbench_module_name}} de-activated.") + "\n")
 
 
